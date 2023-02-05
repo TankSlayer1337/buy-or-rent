8b4e31f7a96c66b1c8d70b1fca3aeb7c81e52fdc
André Säll <andre.sall@scania.com>
2023-02-05 14:34:20 +0100

    Did som renaming and simplified the buy capital calculation.

--- BuyOrRent/BuyOrRent/Program.cs
+++ BuyOrRent/BuyOrRent/Program.cs
@@ -5,44 +5,42 @@
 // https://youtu.be/q9Golcxjpi8
 
 // Buy parameters
-const double downPaymentHouse = 300000d;
-const double houseStartValue = 2000000d;
-const double houseAnnualValueGrowth = 1.03d;
-const double houseLoanInterestMultiplier = 1.03d;
-const double houseMonthlyFee = 4000d;
-const double interestDeductionPercent = 25d;
-const double houseLoanDeductedInterestMultiplier = (houseLoanInterestMultiplier - 1) * (1 - interestDeductionPercent / 100) + 1;
-const double monthlyAmortization = 5000d;
+const double houseStartValue = 3000000d;
+const double downPaymentHouse = houseStartValue * 0.15;
+const double houseValueGrowthMultiplierAnnual = 1.03d;
+const double houseLoanInterestMultiplierAnnual = 1.03d;
+const double houseFeeMonthly = 4000d;
+const double loanInterestDeductionPercent = 25d;
+const double loanDeductedInterestMultiplierAnnual = (houseLoanInterestMultiplierAnnual - 1) * (1 - loanInterestDeductionPercent / 100) + 1;
+const double amortizationMonthly = 5000d;
 
-var monthlyLoanInterestMultiplier = Math.Pow(houseLoanDeductedInterestMultiplier, 1d / 12d);
-var monthlyHouseValueGrowth = Math.Pow(houseAnnualValueGrowth, 1d / 12d);
+var loanDeductedInterestMultiplierMonthly = Math.Pow(loanDeductedInterestMultiplierAnnual, 1d / 12d);
+var houseValueGrowthMultiplierMonthly = Math.Pow(houseValueGrowthMultiplierAnnual, 1d / 12d);
 var houseValue = houseStartValue;
 var houseLoan = houseStartValue - downPaymentHouse;
 
 // Rent parameters
-const double stocksInterestMultiplier = 1.0657d;
+const double stocksInterestMultiplierAnnual = 1.0657d;
 const double rent = 8000;
 
-var monthlyStocksInterestMultiplier = Math.Pow(stocksInterestMultiplier, 1d / 12d);
+var stocksInterestMultiplierMonthly = Math.Pow(stocksInterestMultiplierAnnual, 1d / 12d);
 
 // General parameters
-const double yearsToSimulate = 30;
+const double yearsToSimulate = 20;
 const double monthsToSimulate = yearsToSimulate * 12;
 
-
-var capitalBuy = downPaymentHouse;
 var capitalRent = downPaymentHouse;
 
 var monthsLeftToSimulate = monthsToSimulate;
 while (monthsLeftToSimulate > 0 && houseLoan > 0)
 {
-    capitalBuy += monthlyAmortization;
-    houseLoan -= monthlyAmortization;
-    houseValue *= monthlyHouseValueGrowth;
+    // interest deduction is in reality made on an annual basis, but we simplify it (in favor of buying).
+    var buyExpensesThisMonth = houseFeeMonthly + amortizationMonthly + (loanDeductedInterestMultiplierMonthly - 1) * houseLoan;
+    houseLoan -= amortizationMonthly;
+    houseValue *= houseValueGrowthMultiplierMonthly;
 
-    var buyExpensesThisMonth = houseMonthlyFee + monthlyAmortization + (monthlyLoanInterestMultiplier - 1) * houseLoan;
     var buyRentDifference = buyExpensesThisMonth - rent;
-    capitalRent *= monthlyStocksInterestMultiplier;
+    capitalRent *= stocksInterestMultiplierMonthly;
     if (buyRentDifference > 0)
     {
         capitalRent += buyRentDifference;
@@ -53,7 +51,7 @@ while (monthsLeftToSimulate > 0 && houseLoan > 0)
 
 var houseValueGrowth = houseValue - houseStartValue;
 
-capitalBuy += houseValueGrowth;
+var capitalBuy = houseValue - houseLoan;
 
 Console.WriteLine($"Simulation ran for {Math.Round((monthsToSimulate - monthsLeftToSimulate)/12d, 1)} years.");
 Console.WriteLine($"Start capital: {Convert.ToInt32(downPaymentHouse):n0}");
